e56982c6d9afc9e7c5b07086b2bf870e7a58a1ae
Nathan Rothenberger <nroth10@gmail.com>
2016-03-10 15:29:50 -0500

    Edit themes. Revis js

---
+++ js/scripts.js
@@ -0,0 +1,107 @@
+function videoHandler() {
+	var videoToggle = $('.action--video');
+	var videoCloseBtn = $('.video--close');
+	var videoWrapper = $('.video__wrapper');
+
+	// video toggle
+	$(videoToggle).click( function() {				
+		console.log('toggle video');
+		videoWrapper.toggleClass('video--hidden');		
+		blackOutToggle();
+
+	});
+
+	// close btn, 'x'
+	$(videoCloseBtn).click( function() {
+		videoWrapper.toggleClass('video--hidden');
+		// blackOut();
+		changeTheme();
+	});
+		
+}
+
+videoHandler(); 
+
+// pikc random number
+// var numberOfThemes = 3;
+var themes = [ 'rasta', 'usa', 'rainbow' ]
+// var themes = [ 'rasta', 'usa' ]
+// console.log( themes.length );
+
+
+// pick a random interger up to, not including the highest in the range
+function randomInteger(range){
+	return Math.floor( (Math.random() * range) );
+}
+
+function changeTheme() {
+	
+	var $body = $('body')[0];
+	var numberOfThemes = themes.length;
+	// console.log('number of themes = ' + numberOfThemes);
+	
+	// var randomThemeNumber = randomInteger(numberOfThemes);
+	// console.log('our random theme number is ' + randomThemeNumber);
+
+	// var newTheme = themes[randomThemeNumber];
+	// console.log('our new theme will be ' + newTheme);
+
+	var currentTheme = $body.dataset.theme;
+	console.log('our current theme is ' + currentTheme);
+
+	// if ( currentTheme === newTheme ){
+	// 	console.log('SAME THEME :|')
+		
+	// 	// try again
+	// 	do {
+			
+	// 		console.log('inside while')
+	// 		var randomThemeNumber = randomInteger(numberOfThemes);
+	// 		var newTheme = themes[randomThemeNumber];
+	// 		console.log('our new theme will be ' + newTheme);
+
+	// 	} while ( currentTheme === newTheme );
+
+	// } else {
+	// 	$body.dataset.theme = newTheme;
+	// 	blackOut();
+	// }
+
+	// only while loop
+	do {
+			
+			console.log('inside while')
+			
+			var randomThemeNumber = randomInteger(numberOfThemes);
+			var newTheme = themes[randomThemeNumber];
+			console.log('our new theme will be ' + newTheme);
+
+	} while ( currentTheme === newTheme );
+
+	$body.dataset.theme = newTheme;
+	blackOutToggle();
+	
+}
+
+function blackOut(){
+	// $('body')[0].dataset.blackout = true;
+	console.log('blackout');
+	$('body').toggleClass('blackout');	
+}
+
+function blackOutToggle(){
+	console.log('blackout toggle');
+	$('body').toggleClass('blackout');
+}
+
+// why>?
+function blackOutIf() {
+	var $body = $('body');
+	// var bodyBlackout? = $body.hasClass('blackout');
+	// ????
+	// bodyBlackout ?  : $body.toggleClass('blackout'); 
+}
+
+
+// random();
+
--- notes.md
+++ notes.md
@@ -1,4 +1,17 @@
-h5bp in jade
+[h5bp in jade](https://gist.github.com/xtian/888338/)
 can't use spaces and tabs
 
 had to use typekit for coppper black
+
+
+[thinkful github pages](https://www.thinkful.com/learn/a-guide-to-using-github-pages/start/new-project/project-page/)
+[sync master --> ghpages](http://brettterpstra.com/2012/09/26/github-tip-easily-sync-your-master-to-github-pages/)
+[show hidden files in sublim](http://stackoverflow.com/questions/16640097/show-hidden-folders-in-sublime-text-mac-osx)
+
+order of adding new scss files w/ prepros to avoid errors
+
+
+[data strings in js](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_data_attributes)
+[data2](https://hacks.mozilla.org/2012/10/using-data-attributes-in-javascript-and-css/)
+
+http://encosia.com/3-reasons-why-you-should-let-google-host-jquery-for-you/
